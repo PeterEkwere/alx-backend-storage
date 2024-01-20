fd0fc03982039614ff131d815736828fc055324c
PeterEkwere <peterekwere04@gmail.com>
2024-01-20 16:42:23 +0000

    updated task 0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,5 @@
 -- script that creates a table users
-CREATE TABLE users (
+CREATE TABLE IF NOT EXIST users (
 	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
 	email VARCHAR(255) UNIQUE NOT NULL,
 	name VARCHAR(255)
