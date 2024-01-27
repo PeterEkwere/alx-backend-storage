4a41c89399a752f460d2b99b95803c3a47c144ab
PeterEkwere <Peterekwere04@gmail.com>
2024-01-27 15:02:30 +0000

    initail commit for task 0

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+    THis Module Contains the cache class
+    author: Peter Ekwere
+"""
+import redis
+from typing import Union
+import uuid
+
+
+class Cache:
+    """
+    This is the cache Class
+    """
+
+    def __init__(self: 'Cache') -> None:
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self: 'Cache', data: Union[str, bytes, int, float]) -> str:
+        """
+        THis method  store the input data in Redis using a random key
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
