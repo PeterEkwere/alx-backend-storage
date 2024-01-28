491494b914743a5129cb58ef3c0ea9888bf03cad
PeterEkwere <Peterekwere04@gmail.com>
2024-01-28 21:45:01 +0000

    update 1 for task 5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,29 +1,47 @@
 #!/usr/bin/env python3
 """
-    This Module uses the requests module to obtain the HTML content of a particular URL and returns it.
+    This Module uses the requests module to obtain the HTML content of a URL.
     Author: Peter Ekwere
 """
 import requests
 import redis
-
+import functools
 
 cli = redis.Redis()
 
+
+def track_access_count(url: str) -> None:
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            cli.incr(f"count:{url}")
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
+
+
+def cache_result(url: str, expiration_time: int = 10) -> None:
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            cached_result = cli.get(f"cache:{url}")
+            if cached_result:
+                return cached_result.decode('utf-8')
+
+            result = func(*args, **kwargs)
+            cli.setex(f"cache:{url}", expiration_time, result)
+            return result
+        return wrapper
+    return decorator
+
+
+@track_access_count
+@cache_result
 def get_page(url: str) -> str:
     """
+    This function get_page track how many times a particular
+    URL was accessed in the key "count:{url}" and
+    cache the result with an expiration time of 10 seconds.
     """
-    key = f"count:{url}"
-    cli.incr(key)
-
-    cache_content_key = f"cache:{url}"
-    cached_content = cli.get(cache_content_key)
-
-    if cached_content:
-        return cached_content.decode('utf-8')
-
     resp = requests.get(url)
-    html_content = resp.text
-    
-    cli.setex(cached_content_key, 10, html_content)
-    
-    return html_content
+    return resp.text
