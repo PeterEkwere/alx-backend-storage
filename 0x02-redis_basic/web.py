f686e6ad6b9798cc2a84760f9ace5ee88ffc0c6b
PeterEkwere <Peterekwere04@gmail.com>
2024-01-28 21:53:49 +0000

    update 3 for task 5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -9,39 +9,20 @@ import functools
 
 cli = redis.Redis()
 
-
-def track_access_count(url: str) -> None:
-    def decorator(func):
-        @functools.wraps(func)
-        def wrapper(*args, **kwargs):
-            cli.incr(f"count:{url}")
-            return func(*args, **kwargs)
-        return wrapper
-    return decorator
+# @track_access_count
+# @cache_result
 
 
-def cache_result(url: str, expiration_time: int = 10) -> None:
-    def decorator(func):
-        @functools.wraps(func)
-        def wrapper(*args, **kwargs):
-            cached_result = cli.get(f"cache:{url}")
-            if cached_result:
-                return cached_result.decode('utf-8')
-
-            result = func(*args, **kwargs)
-            cli.setex(f"cache:{url}", expiration_time, result)
-            return result
-        return wrapper
-    return decorator
-
-
-@track_access_count
-@cache_result
 def get_page(url: str) -> str:
     """
     This function get_page track how many times a particular
     URL was accessed in the key "count:{url}" and
     cache the result with an expiration time of 10 seconds.
     """
-    resp = requests.get(url)
-    return resp.text
+    resp = requests.get(url).text
+    if not cli.get("count:{}".format(url)):
+        cli.set("count:{}".format(url), 1)
+        cli.setex("result:{}".format(url), 10, resp)
+    else:
+        cli.incr("count:{}".format(url), 1)
+    return resp
