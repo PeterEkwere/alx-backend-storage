8c79c0b263469a826380c1a6ee998e78f5168f39
PeterEkwere <Peterekwere04@gmail.com>
2024-01-28 00:16:00 +0000

    initail commit for task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@
     author: Peter Ekwere
 """
 import redis
-from typing import Union
+from typing import Union, Callable, Optional
 import uuid
 
 
@@ -24,3 +24,27 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            fn: Callable = None) -> Union[str, bytes, int, float]:
+        """
+        This Method gets the corresponding value from the redis database
+        """
+        if fn:
+            value = fn(self._redis.get(key))
+            return value
+        return self._redis.get(key)
+
+    def get_str(self, value: bytes) -> str:
+        """
+        This method converts bytes to a string
+        """
+        string_value = value.decode('utf-8')
+        return string_value
+
+    def get_int(self, value: bytes) -> int:
+        """
+        This method converts a byte values to integers
+        """
+        int_value = int(value)
+        return int_value
